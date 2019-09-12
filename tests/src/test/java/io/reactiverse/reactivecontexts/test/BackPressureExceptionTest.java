29bb6ba2ee510ef13c09286b8dc363240e72c942
Stephane Epardaud <stef@epardaud.fr>
2019-09-12 11:27:40 +0200

    Added test for rxjava2

--- tests/src/test/java/io/reactiverse/reactivecontexts/test/BackPressureExceptionTest.java
+++ tests/src/test/java/io/reactiverse/reactivecontexts/test/BackPressureExceptionTest.java
@@ -1,15 +1,15 @@
 package io.reactiverse.reactivecontexts.test;
 
 import io.reactiverse.reactivecontexts.core.Context;
+import io.reactivex.observers.TestObserver;
+import rx.observers.AssertableSubscriber;
+
 import org.junit.BeforeClass;
 import org.junit.Test;
-import rx.observers.AssertableSubscriber;
 
 import java.util.concurrent.TimeUnit;
 
 import static java.util.Arrays.asList;
-import static rx.Observable.from;
-import static rx.Observable.just;
 
 public class BackPressureExceptionTest {
 
@@ -20,15 +20,28 @@ public class BackPressureExceptionTest {
 
 
     @Test
-    public void testBackPressure() {
+    public void testBackPressureRxJava1() {
         AssertableSubscriber<Integer> test =
-            from(asList(1,2,3,4,5,6))
-                .concatMap(integer -> just(integer).delay(100, TimeUnit.MILLISECONDS))
+            rx.Observable.from(asList(1,2,3,4,5,6))
+                .concatMap(integer -> rx.Observable.just(integer).delay(100, TimeUnit.MILLISECONDS))
                 .test();
 
         test.awaitTerminalEvent();
 
         test.assertNoErrors();
         test.assertReceivedOnNext(asList(1,2,3,4,5,6));
+    }
+
+    @Test
+    public void testBackPressureRxJava2() {
+        TestObserver<Integer> test =
+            io.reactivex.Observable.just(1,2,3,4,5,6)
+                .concatMap(integer -> io.reactivex.Observable.just(integer).delay(100, TimeUnit.MILLISECONDS))
+                .test();
+
+        test.awaitTerminalEvent();
+
+        test.assertNoErrors();
+        test.assertValues(1,2,3,4,5,6);
     }
 }
